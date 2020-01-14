1f88b57c17d1f2bdc31b9dd1f9bcbc5f1fe489f3
TheUnicum <tia_ben@tin.it>
2020-01-14 17:00:40 +0100

    [T07] - (13:16) - <some basic windows error checks> BIS

--- CTN_05_Hardware_3D_DirectX/src/Window.cpp
+++ CTN_05_Hardware_3D_DirectX/src/Window.cpp
@@ -143,7 +143,7 @@ Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
 	hr(hr)
 {}
 
-const char* Window::Exception::what() const noexcept
+const char * Window::Exception::what() const noexcept
 {
 	std::ostringstream oss;
 	oss << GetType() << std::endl
@@ -162,17 +162,21 @@ const char* Window::Exception::GetType() const noexcept
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
 	char* pMsgBuf = nullptr;
+	// windows will allocate memory for err string and make our pointer point to it
 	DWORD nMsgLen = FormatMessage(
 		FORMAT_MESSAGE_ALLOCATE_BUFFER |
 		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 		nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 		reinterpret_cast<LPSTR>(&pMsgBuf), 0, nullptr
 	);
+	// 0 string length returned indicates a failure
 	if (nMsgLen == 0)
 	{
 		return "Unidentified error code";
 	}
+	// copy error string from windows-allocated buffer to std::string
 	std::string errorString = pMsgBuf;
+	// free windows buffer
 	LocalFree(pMsgBuf);
 	return errorString;
 }
