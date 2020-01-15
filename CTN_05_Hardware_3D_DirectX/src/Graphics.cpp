24cac12dc4949a507219dd3942bf06413663b80f
TheUnicum <tia_ben@tin.it>
2020-01-15 17:20:05 +0100

    [T16.2] - (0:0) - <pixel shader set and loaded>

--- CTN_05_Hardware_3D_DirectX/src/Graphics.cpp
+++ CTN_05_Hardware_3D_DirectX/src/Graphics.cpp
@@ -144,6 +144,15 @@ void Graphics::DrawTestTriangle()
 	pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
 
 
+	//create a pixel shader
+	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+	GFX_THROW_INFO(D3DReadFileToBlob(L"PixelShader.cso", &pBlob));
+	GFX_THROW_INFO(pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader));
+
+	// bind pixel shader
+	pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
+
+
 	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
 }
 
