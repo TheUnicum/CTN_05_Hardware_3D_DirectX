d0c027ba86cdb0832865c10951e496352ff2115e
TheUnicum <tia_ben@tin.it>
2020-03-06 12:20:41 +0100

    [T36] - (22:32) - <fix testplane nmap slot>

--- CTN_05_Hardware_3D_DirectX/src/TestPlane.cpp
+++ CTN_05_Hardware_3D_DirectX/src/TestPlane.cpp
@@ -16,7 +16,7 @@ TestPlane::TestPlane(Graphics& gfx, float size)
 	AddBind(IndexBuffer::Resolve(gfx, geometryTag, model.indices));
 
 	AddBind(Texture::Resolve(gfx, "src\\Images\\brickwall.jpg"));
-	AddBind(Texture::Resolve(gfx, "src\\Images\\brickwall_normal_obj.jpg", 1u));
+	AddBind(Texture::Resolve(gfx, "src\\Images\\brickwall_normal_obj.jpg", 2u));
 
 	auto pvs = VertexShader::Resolve(gfx, "PhongVS.cso");
 	auto pvsbc = pvs->GetBytecode(); 
