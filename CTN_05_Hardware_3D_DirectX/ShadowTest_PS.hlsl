01f78c2f4ddeb60eb3f0cb95b1d3860d3122eb9f
TheUnicum <tia_ben@tin.it>
2020-10-27 10:35:36 +0100

    [T53] - (9:24) - <issue with floor shadow reception>

--- CTN_05_Hardware_3D_DirectX/ShadowTest_PS.hlsl
+++ CTN_05_Hardware_3D_DirectX/ShadowTest_PS.hlsl
@@ -12,16 +12,17 @@ cbuffer ObjectCBuf : register(b1)
 };
 
 Texture2D tex : register(t0);
+
 SamplerState splr : register(s0);
 
 
-float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float2 tc : Texcoord, float4 spos : ShadowPosition) : SV_Target
+float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float2 tc : Texcoord, float3 spos : ShadowPosition) : SV_Target
 {
 	float3 diffuse;
 	float3 specular;
-
+	
 	// shadow map test
-    if (ShadowUnoccluded(spos))
+	if (ShadowUnoccluded(spos))
 	{
 		// renormalize interpolated normal
 		viewNormal = normalize(viewNormal);
@@ -36,7 +37,7 @@ float4 main(float3 viewFragPos : Position, float3 viewNormal : Normal, float2 tc
 	}
 	else
 	{
-		diffuse = specular = float3(0.0f, 0.0f, 0.0f);
+		diffuse = specular = 0.0f;
 	}
 	// final color
 	return float4(saturate((diffuse + ambient) * tex.Sample(splr, tc).rgb + specular), 1.0f);
